d8dd383f209d50da813a08a4dd8e9d269c4b1e24
Forkipe <mhorkiy@gmail.com>
2022-10-09 21:31:35 +0300

    Initial Commit

---
+++ Course/CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseApp.swift
+//  Course
+//
+//  Created by Марк Горкій on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
